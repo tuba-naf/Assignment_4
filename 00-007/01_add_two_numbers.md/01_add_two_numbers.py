a1093f12840ceafb7dd9ca97c1b2fec99c363c45
Tuba Nafees <tuba.nafees4@gmail.com>
2025-04-05 08:36:04 +0500

    Assignment Pushes

---
+++ 00-007/01_add_two_numbers.md/01_add_two_numbers.py
@@ -0,0 +1,10 @@
+def main():
+    num1 = input("Enter the first number: ")
+    num1 = int(num1)
+    num2 = input("Enter the second number: ")
+    num2 = int(num2)
+    total_sum = num1 + num2
+    print(f"The sum of two numbers is {total_sum}")
+
+if __name__ == '__main__':
+    main()
---
+++ 00-007/02_agreement_bot.md/02_agreement_bot.py
@@ -0,0 +1,5 @@
+def main():
+    animal = input("\033[1;3m What is your favourite animal? ")
+    print(f"My favourite animal is {animal}.")
+if __name__ == '__main__':
+    main()
